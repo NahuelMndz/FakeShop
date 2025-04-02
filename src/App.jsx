2a2d83efdd30faa91266eb5d59f3b10dbc1c496d
NahuelMndz <nahuelcolonn12@gmail.com>
2025-04-01 23:20:43 -0300

    quinto-commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Router>
         <Navbar/>
           <Routes>
-            <Route path='/' element={<Main/>} />
+            <Route path='/FakeShop' element={<Main/>} />
             <Route path='/product/:id' element={<Products/>} />
           </Routes>
         </Router>
